e16711cc3a9a30e69c1d3a2599fb012ad0d0e0d9
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-23 19:36:42 -0300

    DTO

--- socialmedia/src/main/java/com/leo/socialmedia/config/Instantiation.java
+++ socialmedia/src/main/java/com/leo/socialmedia/config/Instantiation.java
@@ -1,4 +1,27 @@
 package com.leo.socialmedia.config;
 
-public class Instantiation {
+import com.leo.socialmedia.domain.User;
+import com.leo.socialmedia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User("maria@gmail.com", null,  "Maria Brown");
+        User alex = new User("alex@gmail.com", null, "Alex Green");
+        User bob = new User("bob@gmail.com",null, "Bob Grey");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
 }
--- socialmedia/src/main/java/com/leo/socialmedia/resources/UserResource.java
+++ socialmedia/src/main/java/com/leo/socialmedia/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.leo.socialmedia.resources;
 
 import com.leo.socialmedia.domain.User;
+import com.leo.socialmedia.dto.UserDTO;
 import com.leo.socialmedia.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
