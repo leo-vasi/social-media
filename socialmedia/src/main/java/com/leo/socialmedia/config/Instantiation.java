f8f5f82696341f64545225b1557cc3394c194021
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-25 15:58:21 -0300

    User posts reference

--- socialmedia/src/main/java/com/leo/socialmedia/config/Instantiation.java
+++ socialmedia/src/main/java/com/leo/socialmedia/config/Instantiation.java
@@ -40,5 +40,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post("Vou viajar para São Paulo", sdf.parse("23/03/2018"), null, "Bom dia", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
