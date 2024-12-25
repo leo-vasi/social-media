791493e99138ca9179a08c32629c42ba2f5989f1
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-25 19:07:27 -0300

    more queries

--- socialmedia/src/main/java/com/leo/socialmedia/service/PostService.java
+++ socialmedia/src/main/java/com/leo/socialmedia/service/PostService.java
@@ -6,6 +6,7 @@ import com.leo.socialmedia.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,8 +24,12 @@ public class PostService {
         return repository.findByTitle(text);
     }
 
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
+    }
 
-    
+
 
 
 }
